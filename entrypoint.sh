dcb3b046a031d20920f1ca8d91470cdc42c184e4
Vitaly Uvarov <vitalyu@gmail.com>
2018-02-02 17:50:03 +0300

    entrypoint script fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,18 @@ BUILD_DIR="/data/_build"
 [[ ! -f "data.yml" ]] && echo "[E] No data.yml file presented." && echo '[!] Try to run container with mount option, eg: docker run --rm -v "$(pwd)":/data docker-jinja2' && exit 1;
 
 echo "[~] Cleanup _build directory"
-rm -rf ${BUILD_DIR}/* &2> /dev/null
+rm -rf "${BUILD_DIR}" 2> /dev/null || true
 
-for template in $( find . -name '*.build.j2' -not -path "${BUILD_DIR}/*" ) ; do
+for template in $( cd /data/ && find . -name '*.build.j2' | grep -v "./_build" ) ; do
     echo "[~] Generating ${template} to ${template%.build.j2}.conf"
     install -d $( dirname "${BUILD_DIR}/${template%.build.j2}.conf" )
     python /pj.py "${template}" > "${BUILD_DIR}/${template%.build.j2}.conf"
 done
 
-for extra_file in $( find . -name '*.conf' -not -path "${BUILD_DIR}/*" ) ; do
+for extra_file in $( cd /data/ && find . -name '*.conf' | grep -v "./_build" ) ; do
     echo "[~] Copying ${extra_file}"
     install -d $( dirname "${BUILD_DIR}/${extra_file}" )
     cp -f "${extra_file}" "${BUILD_DIR}/${extra_file}"
 done
+
 echo "[.] Done"
