2c467e5d8ac505f768424e1f043c757ff0315c4b
Vitaly <vitalyu@gmail.com>
2018-02-08 12:14:53 +0300

    Python and Jinja versions show at start

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 set -e
 BUILD_DIR="/data/_build"
 
+echo "[!] $(python --version)"
+echo "[!] Jinja2 $(pip show Jinja2 | grep Version)"
+
 [[ ! -f "data.yml" ]] && echo "[E] No data.yml file presented." && echo '[!] Try to run container with mount option, eg: docker run --rm -v "$(pwd)":/data docker-jinja2' && exit 1;
 
 echo "[~] Cleanup _build directory"
